9c5d0f4aaf65f986208c4ea0a7d31a9260e736f0
Zaid Al Habbal <alhabbalzaid10@gmail.com>
2025-03-31 12:50:49 +0300

    add django auth views

--- dj_project/urls.py
+++ dj_project/urls.py
@@ -1,8 +1,9 @@
 """
-URL configuration for dj_project project.
+URL configuration for educa project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+The `urlpatterns` list routes URLs to views. For more information
+please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -11,19 +12,29 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import the include() function: from django.urls import include,
+        path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path(
+        'accounts/login/', auth_views.LoginView.as_view(), name='login'
+    ),
+    path(
+        'accounts/logout/',
+        auth_views.LogoutView.as_view(),
+        name='logout',
+    ),
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
