fb43ba833fccea2fdf77ca83715c701e6a48cbe0
Zaid Al Habbal <alhabbalzaid10@gmail.com>
2025-04-02 17:50:14 +0300

    use viewsets

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics
-from courses.api.serializers import SubjectSerializer
-from courses.models import Subject
 from django.db.models import Count
+from rest_framework import generics
+from rest_framework import viewsets
+
+from courses.api.serializers import SubjectSerializer, CourseSerializer
+from courses.models import Subject, Course
 from courses.api.pagination import StandardPagination
 
 class SubjectListView(generics.ListAPIView):
@@ -14,3 +16,14 @@ class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
 
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
+    serializer_class = CourseSerializer
+    pagination_class = StandardPagination
+
+
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
+    serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
