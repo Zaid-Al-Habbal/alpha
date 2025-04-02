a965b4ab756f6a34ff585dcd2e66ab99fb899ceb
Zaid Al Habbal <alhabbalzaid10@gmail.com>
2025-04-02 12:26:09 +0300

    add students app

--- dj_project/urls.py
+++ dj_project/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
         path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from courses.views import CourseListView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path(
@@ -32,6 +33,10 @@ urlpatterns = [
         name='logout',
     ),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
+    path('', CourseListView.as_view(), name='course_list'),
+    path('students/', include('students.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
 
 if settings.DEBUG:
