d92d2e1c9b6cb448ccb79884549d9f888f7f9d13
Zaid Al Habbal <alhabbalzaid10@gmail.com>
2025-04-04 15:04:56 +0300

    update add action to CourseViewSet

--- courses/api/views.py
+++ courses/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 
 from courses.api.serializers import SubjectSerializer, CourseSerializer
 from courses.models import Subject, Course
@@ -28,6 +29,20 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = CourseSerializer
     pagination_class = StandardPagination
 
+    @action(
+        detail=True,
+        methods=['post'],
+        authentiaction_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated]
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response(
+            {
+                'enrolled': True
+            }
+        )
 
 class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
@@ -38,7 +53,7 @@ class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
 class CourseEnrollView(APIView):
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
-    
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
