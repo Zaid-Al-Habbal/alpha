a696e1ccc3f9dad6d9f1ab9c902a925531d05064
Zaid Al Habbal <alhabbalzaid10@gmail.com>
2025-04-04 14:45:58 +0300

    enroll student in a course using custom view

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -8,23 +8,34 @@ class SubjectSerializer(serializers.ModelSerializer):
     popular_courses = serializers.SerializerMethodField()
 
     def get_popular_courses(self, sub_obj):
-        courses = sub_obj.courses.annotate(total_students=Count('students')).order_by('total_students')[:3]
-        return [
-            f'{c.title} ({c.total_students})' for c in courses
-        ]
+        courses = sub_obj.courses.annotate(total_students=Count("students")).order_by(
+            "total_students"
+        )[:3]
+        return [f"{c.title} ({c.total_students})" for c in courses]
 
     class Meta:
         model = Subject
-        fields = ['id', 'title', 'slug', 'total_courses', 'popular_courses']
+        fields = ["id", "title", "slug", "total_courses", "popular_courses"]
+
 
 class ModuleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Module
-        fields = ['order', 'title', 'description']
+        fields = ["order", "title", "description"]
+
 
 class CourseSerializer(serializers.ModelSerializer):
-    modules = ModuleSerializer(many=True,read_only=True)
+    modules = ModuleSerializer(many=True, read_only=True)
+
     class Meta:
         model = Course
-        fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules', ]
-        
+        fields = [
+            "id",
+            "subject",
+            "title",
+            "slug",
+            "overview",
+            "created",
+            "owner",
+            "modules",
+        ]
--- courses/api/urls.py
+++ courses/api/urls.py
@@ -13,6 +13,6 @@ router.register('subjects', views.SubjectViewSet)
 urlpatterns = [
     # path('subjects/', views.SubjectListView.as_view(), name='subject_list'),
     # path('subjects/<pk>/', views.SubjectDetailView.as_view(), name='subject_detail'),
-    path('', include(router.urls))
-
+    path('', include(router.urls)),
+    path('courses/<pk>/enroll/', views.CourseEnrollView.as_view(), name='course_enroll')
 ]
